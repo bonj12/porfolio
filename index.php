511a7b2a38bf2c03356cc104b9c2ebc7e9e9cf80
bonj12 <bonjeronaldbarry12@gmail.com>
2023-02-06 03:38:44 +0800

    .

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex flex-column justify-content-center">
     <div class="container" data-aos="zoom-in" data-aos-delay="100">
-      <h1>Brandon Johnson</h1>
+      <h1>Ronald Barry Bonje</h1>
       <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
       <div class="social-links">
         <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
@@ -87,7 +87,7 @@
             <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-8 pt-4 pt-lg-0 content">
-            <h3>UI/UX Designer &amp; Web Developer.</h3>
+            <h3>Fullstack Webapp Developer</h3>
             <p class="fst-italic">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
               magna aliqua.
@@ -95,18 +95,17 @@
             <div class="row">
               <div class="col-lg-6">
                 <ul>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>January 6, 1994</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>https://ronald-bonje.herokuapp.com/</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+63 991 364 8515</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Cebu City, Philippines</span></li>
                 </ul>
               </div>
               <div class="col-lg-6">
                 <ul>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>29</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>College Graduage</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>bonjeronaldbarry12@gmail.com</span></li>
                 </ul>
               </div>
             </div>
@@ -245,55 +244,101 @@
 
         <div class="row">
           <div class="col-lg-6">
-            <h3 class="resume-title">Sumary</h3>
-            <div class="resume-item pb-0">
-              <h4>Brandon Johnson</h4>
-              <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
+            <!-- <h3 class="resume-title">Sumary</h3> -->
+            
+            <div class="resume-item">
+              <h4>Web developer - URG-Asia</h4>
+              <h5>July 2018 - January 2019</h5>
+              <p><em>Talamban Cebu (Satellite Office) based from Australia </em></p>
               <ul>
-                <li>Portland par 127,Orlando, FL</li>
-                <li>(123) 456-7891</li>
-                <li>alice.barkley@example.com</li>
+                <li>Create and Maintain Wordpress Website</li>
+                <li>Develop and Enhance Website from Native language to Framework (CodeIgniter) </li>
+                <li>CPanel Management</li>
               </ul>
             </div>
 
-            <h3 class="resume-title">Education</h3>
             <div class="resume-item">
-              <h4>Master of Fine Arts &amp; Graphic Design</h4>
-              <h5>2015 - 2016</h5>
-              <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-              <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+              <h4>Web Developer - Goodapps INC.</h4>
+              <h5>March 2019 - December 2019</h5>
+              <p><em>Advent Business Center, 139 Acacia St, Cebu City, 6000 Cebu</em></p>
+              <ul>
+                <li>Develop and Enhanced System used by GAISANO CAPITAL (Purchase Order System)</li>
+                <li>Enhanced and Debug the back-end processes</li>
+                <li>Create back-end processes that create Database Table on specific date and time</li>
+                <li>Maintain Sms System</li>
+              </ul>
             </div>
+
             <div class="resume-item">
-              <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-              <h5>2010 - 2014</h5>
-              <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-              <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+              <h4>BigBrain Digital Marketing</h4>
+              <h5>September 2019 - March 2020</h5>
+              <p><em>The Space Building Banilad, Mandaue</em></p>
+              <ul>
+                <li>Develop Wordpress Websites</li>
+                <li>Manage Admin side / Cpanel</li>
+              </ul>
+            </div>
+
+            <div class="resume-item">
+              <h4>Nouveau. Alliance Group</h4>
+              <h5>April 2020 - July 2020</h5>
+              <p><em>Homebased</em></p>
+              <ul>
+                <li>Develop E-Commerce Websites</li>
+                <li>Create REST-API for Mobile Application</li>
+                <li>Develop Admin Dashboard for system management (front/back)</li>
+                <li>Create and Plan the database for the mobile application</li>
+                <li>Integrate 3rd party API’s</li>
+              </ul>
             </div>
           </div>
           <div class="col-lg-6">
-            <h3 class="resume-title">Professional Experience</h3>
             <div class="resume-item">
-              <h4>Senior graphic design specialist</h4>
-              <h5>2019 - Present</h5>
-              <p><em>Experion, New York, NY </em></p>
-              <ul>
-                <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
-                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-                <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
-                <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
-              </ul>
-            </div>
-            <div class="resume-item">
-              <h4>Graphic design specialist</h4>
-              <h5>2017 - 2018</h5>
-              <p><em>Stepping Stone Advertising, New York, NY</em></p>
-              <ul>
-                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-                <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
-              </ul>
-            </div>
+                <h4>CLoudesk Pty Ltd</h4>
+                <h5>September 2020 - August 2021</h5>
+                <p><em>Homebased/Hybrid Schedule</em></p>
+                <ul>
+                  <li>Create REST-API’s</li>
+                  <li>Develop Web Admin Portal</li>
+                  <li>Debug, Maintain and Enhance old codes</li>
+                  <li>Create lambda functions (AWS)</li>
+                  <li>Manage Databases</li>
+                </ul>
+              </div>
+              <div class="resume-item">
+                <h4>MVP.dev</h4>
+                <h5>July 2021 - January 2022</h5>
+                <p><em>Homebased</em></p>
+                <ul>
+                  <li>Create REST-API’s</li>
+                  <li>Develop Web Admin Portal</li>
+                  <li>Debug, Maintain and Enhance old codes</li>
+                  <li>Create lambda functions (AWS)</li>
+                  <li>Manage Databases</li>
+                </ul>
+              </div>
+              <div class="resume-item">
+                <h4>Web Developer - Goodapps INC.</h4>
+                <h5>March 2019 - December 2019</h5>
+                <p><em>Advent Business Center, 139 Acacia St, Cebu City, 6000 Cebu</em></p>
+                <ul>
+                  <li>Develop and Enhanced System used by GAISANO CAPITAL (Purchase Order System)</li>
+                  <li>Enhanced and Debug the back-end processes</li>
+                  <li>Create back-end processes that create Database Table on specific date and time</li>
+                  <li>Maintain Sms System</li>
+                </ul>
+              </div>
+              <div class="resume-item">
+                <h4>Web Developer - Goodapps INC.</h4>
+                <h5>March 2019 - December 2019</h5>
+                <p><em>Advent Business Center, 139 Acacia St, Cebu City, 6000 Cebu</em></p>
+                <ul>
+                  <li>Develop and Enhanced System used by GAISANO CAPITAL (Purchase Order System)</li>
+                  <li>Enhanced and Debug the back-end processes</li>
+                  <li>Create back-end processes that create Database Table on specific date and time</li>
+                  <li>Maintain Sms System</li>
+                </ul>
+              </div>
           </div>
         </div>
 
@@ -701,7 +746,7 @@
   <!-- ======= Footer ======= -->
   <footer id="footer">
     <div class="container">
-      <h3>Brandon Johnson</h3>
+      <h3>Ronald Barry Bonje</h3>
       <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
       <div class="social-links">
         <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
